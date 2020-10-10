2b9fbf096d117500ee11bc6fad0b671668d819e7
Dmitry Kutlenkov <kutlenkov.dmitri@gmail.com>
2020-10-10 19:58:21 +0300

    Better structure of Applier

--- src/main/kotlin/SequentialApplier.kt
+++ src/main/kotlin/SequentialApplier.kt
@@ -3,43 +3,44 @@ import IntentionHandler.Companion.file
 import IntentionHandler.Companion.project
 import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.psi.PsiDocumentManager
-import com.intellij.psi.impl.PsiDocumentManagerImpl
 
 
 class SequentialApplier {
     var events = mutableListOf<IntentionEvent>()
-    var hashes = mutableSetOf<Int>()
+    private var hashes = mutableSetOf<Int>()
+    private val document = editor!!.document // Is it okay to put it here?
+
+    // Explanation of the following function: https://jetbrains.org/intellij/sdk/docs/basics/architectural_overview/general_threading_rules.html
+    private fun runWriteCommandAndCommit(command: () -> Unit) {
+        WriteCommandAction.runWriteCommandAction(project) { command()
+            PsiDocumentManager.getInstance(project!!).commitDocument(document)
+        }
+    }
 
     fun start() {
         val actions = IntentionHandler.getIntentionsList(true)
-        val document = editor!!.document
+
         val oldText = document.text
         if (actions.isEmpty()) return
 
         for (action in actions) {
-            println(action)
-            println(oldText)
+//            println(action)
+//            println(oldText)
 
-            // Explanation of the following command: https://jetbrains.org/intellij/sdk/docs/basics/architectural_overview/general_threading_rules.html#invokelater
+
             val intentionName = IntentionHandler.getIntentionActionByName(action)
-
-            WriteCommandAction.runWriteCommandAction(project) { intentionName.invoke(project!!, editor, file)
-                PsiDocumentManager.getInstance(project!!).commitDocument(document)}
+            runWriteCommandAndCommit{ intentionName.invoke(project!!, editor, file) }
 
             val event = IntentionEvent(action, oldText.hashCode(), document.text.hashCode())
             events.add(event)
             if (event.hash_start !in hashes) hashes.add(event.hash_start)
             if (event.hash_end in hashes) {
                 println("hash ${event.hash_end} is in hashes, returning")
-                return // Test purposes, TODO change to continue
+                continue
             } else {
                 hashes.add(event.hash_end)
                 start()
-                WriteCommandAction.runWriteCommandAction(project) {
-//                    document.replaceString(0, document.textLength, oldText)
-                    document.setText(oldText)
-                    PsiDocumentManager.getInstance(project!!).commitDocument(document)
-                } // Replace code entirely with previous one
+                runWriteCommandAndCommit { document.setText(oldText) } // Replace code entirely with previous one
 
             }
         }
