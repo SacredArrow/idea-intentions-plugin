1b1d3ba2b3968dbfa4fcca7f3e31998955eb7c2f
Dmitry Kutlenkov <kutlenkov.dmitri@gmail.com>
2020-10-14 15:11:30 +0300

    Caret position fixed

--- src/main/kotlin/SequentialApplier.kt
+++ src/main/kotlin/SequentialApplier.kt
@@ -5,12 +5,14 @@ import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.psi.PsiDocumentManager
 import java.io.File
 
+data class CodeState(var code: String, var offset: Int)
 
 class SequentialApplier {
     var events = mutableListOf<IntentionEvent>()
     private var hashes = HashMap<Int, String>()
     private val document = editor!!.document // Is it okay to put it here?
     private val docManager = PsiDocumentManager.getInstance(project!!)
+    private val caret = editor!!.caretModel
 
     // Explanation of the following function: https://jetbrains.org/intellij/sdk/docs/basics/architectural_overview/general_threading_rules.html
     private fun runWriteCommandAndCommit(command: () -> Unit) {
@@ -23,22 +25,24 @@ class SequentialApplier {
     fun start() {
         val actions = IntentionHandler.getIntentionsList(true)
 
-        val oldCode = document.text
+        val oldState = CodeState(document.text, caret.offset)
 
         for (action in actions) {
             val intention = IntentionHandler.getIntentionActionByName(action)
-//            println(document.text)
             runWriteCommandAndCommit { intention.invoke(project!!, editor, file) }
             val newCode = document.text
-            val event = IntentionEvent(action, oldCode.hashCode(), newCode.hashCode())
+            val event = IntentionEvent(action, oldState.code.hashCode(), newCode.hashCode())
             events.add(event)
             println(event)
-            if (event.hash_start !in hashes.keys) hashes[event.hash_start] = oldCode
+            if (event.hash_start !in hashes.keys) hashes[event.hash_start] = oldState.code
             if (event.hash_end !in hashes.keys) {
                 hashes[event.hash_end] = newCode
                 start()
             }
-            runWriteCommandAndCommit { document.setText(oldCode) } // Replace code with old one
+            runWriteCommandAndCommit {
+                document.setText(oldState.code)
+                caret.moveToOffset(oldState.offset)
+            } // Replace code with old one
         }
     }
 
